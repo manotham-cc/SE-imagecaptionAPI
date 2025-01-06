54a5d96ef24e3c7d77a84d95d8dfc0306ee2c739
manotham-cc <82450820+manotham-cc@users.noreply.github.com>
2025-01-06 23:04:07 +0700

    FirstCommit

---
+++ imagecaption.py
@@ -0,0 +1,41 @@
+import base64
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+# Load the .env file
+load_dotenv()
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+# Path to your image
+image_path = "image\salmon.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Display only the nutrients and their approximate quantities(not in a range) in this food in JSON format.",
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                },
+            ],
+        }
+    ],
+)
+
+x = (response.choices[0].message.content)
+print(x)
